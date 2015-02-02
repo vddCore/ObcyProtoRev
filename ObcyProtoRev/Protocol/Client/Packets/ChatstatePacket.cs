16ed80d5e22b1d7e31d00e7ec8031daf6f431eb7
Ciastex <mrciastexx@gmail.com>
2015-02-02 20:50:10 +0100

    Added Chatstate and Message packets.

---
+++ ObcyProtoRev/Protocol/Client/Packets/ChatstatePacket.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using ObcyProtoRev.Protocol.SockJs;
+
+namespace ObcyProtoRev.Protocol.Client.Packets
+{
+    class ChatstatePacket : Packet
+    {
+        public ChatstatePacket(bool typing, string contactGuid)
+        {
+            Header = "_mtyp";
+
+            Data = new JObject();
+            Data["ckey"] = contactGuid;
+            Data["val"] = typing;
+        }
+    }
+}
---
+++ ObcyProtoRev/Protocol/Client/Packets/MessagePacket.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using ObcyProtoRev.Protocol.SockJs;
+
+namespace ObcyProtoRev.Protocol.Client.Packets
+{
+    class MessagePacket : Packet
+    {
+        public MessagePacket(string message, string contactGuid)
+        {
+            Header = "_pmsg";
+
+            Data = new JObject();
+            Data["ckey"] = contactGuid;
+            Data["msg"] = message;
+        } 
+    }
+}
