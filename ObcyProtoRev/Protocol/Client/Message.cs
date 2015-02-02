bfd2642f37f58f384b046ecc66d1f6ce7b29b47a
Ciastex <mrciastexx@gmail.com>
2015-02-02 12:05:07 +0100

    Added MessageType enumeration and extended Message class.

--- ObcyProtoRev/Protocol/Client/Message.cs
+++ ObcyProtoRev/Protocol/Client/Message.cs
@@ -2,12 +2,14 @@
 {
     public class Message
     {
+        public MessageType Type { get; private set; }
         public string Body { get; private set; }
         public int ClientId { get; private set; }
         public int PostId { get; private set; }
 
-        public Message(string body, int clientId, int postId)
+        public Message(MessageType type, string body, int clientId, int postId)
         {
+            Type = type;
             Body = body;
             ClientId = clientId;
             PostId = postId;
