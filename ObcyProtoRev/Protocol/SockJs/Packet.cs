51a9f3bef2beaeb592d8dc23d1cecb43412b2c38
Ciastex <mrciastexx@gmail.com>
2015-02-02 15:56:36 +0100

    Added low-level Packet class and PacketType enum.

---
+++ ObcyProtoRev/Protocol/SockJs/Packet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ObcyProtoRev.Protocol.SockJs
+{
+    class Packet : JObject
+    {
+        public string Header
+        {
+            get { return base["ev_name"].ToString(); }
+            set { base["ev_name"] = value; }
+        }
+
+        public JToken Data
+        {
+            get { return base["ev_data"]; }
+            set { base["ev_data"] = value; }
+        }
+
+        public Dictionary<string, JToken> AdditionalFields { get; set; }
+
+        public Packet()
+        {
+            Header = "";
+            AdditionalFields = new Dictionary<string, JToken>();
+        }
+
+        public Packet(string header)
+            : this()
+        {
+            Header = header;
+        }
+
+        public Packet(string header, JToken data)
+            : this(header)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            var jArray = new JArray(ToString(Formatting.None));
+            return jArray.ToString(Formatting.None);
+        }
+    }
+}
