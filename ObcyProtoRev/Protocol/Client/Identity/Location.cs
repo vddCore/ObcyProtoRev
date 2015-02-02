aab16b96bf702d913e0d52eea724f97ede10235d
Ciastex <mrciastexx@gmail.com>
2015-02-02 21:42:35 +0100

    Added identity classes as well as stranger search initiation packet.

---
+++ ObcyProtoRev/Protocol/Client/Identity/Location.cs
@@ -0,0 +1,24 @@
+﻿namespace ObcyProtoRev.Protocol.Client.Identity
+{
+    public enum Location
+    {
+        WholePoland = 0,
+        Podlaskie = 1,
+        Dolnoslaskie = 2,
+        Podkarpackie = 3,
+        KujawskoPomorskie = 4,
+        Pomorskie = 5,
+        Lubelskie = 6,
+        Slaskie = 7,
+        Lubuskie = 8,
+        Swietokrzyskie = 9,
+        Lodzkie = 10,
+        WarminskoMazurskie = 11,
+        Malopolskie = 12,
+        Wielkopolskie = 13,
+        Mazowieckie = 14,
+        Zachodniopomorskie = 15,
+        Opolskie = 16,
+        OutsidePoland = 17
+    }
+}
---
+++ ObcyProtoRev/Protocol/Client/Packets/StrangerSearchPacket.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using ObcyProtoRev.Protocol.Client.Identity;
+using ObcyProtoRev.Protocol.SockJs;
+
+namespace ObcyProtoRev.Protocol.Client.Packets
+{
+    class StrangerSearchPacket : Packet
+    {
+        public StrangerSearchPacket(PersonInfo myInfo, PersonInfo preferencesInfo, string channel)
+        {
+            Header = "_sas";
+
+            var jObjectMyInfo = new JObject();
+            jObjectMyInfo["sex"] = myInfo.Sex;
+            jObjectMyInfo["loc"] = (int)myInfo.Location;
+
+            var jObjectPreferencesInfo = new JObject();
+            jObjectPreferencesInfo["sex"] = preferencesInfo.Sex;
+            jObjectPreferencesInfo["loc"] = (int)preferencesInfo.Location;
+
+            Data = new JObject();
+            Data["channel"] = channel;
+            Data["myself"] = jObjectMyInfo;
+            Data["preferences"] = jObjectPreferencesInfo;
+        }
+    }
+}
