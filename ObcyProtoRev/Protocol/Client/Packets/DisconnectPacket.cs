7f4dbd06db676815f73bee234c53907b04f8b663
Ciastex <mrciastexx@gmail.com>
2015-02-02 20:13:46 +0100

    Added Disconnect packet

---
+++ ObcyProtoRev/Protocol/Client/Packets/DisconnectPacket.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using ObcyProtoRev.Protocol.SockJs;
+
+namespace ObcyProtoRev.Protocol.Client.Packets
+{
+    class DisconnectPacket : Packet
+    {
+        public DisconnectPacket(string contactGuid)
+        {
+            Header = "_distalk";
+
+            var jObject = new JObject();
+            jObject["ckey"] = contactGuid;
+        }
+    }
+}
