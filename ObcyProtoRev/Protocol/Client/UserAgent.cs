3e5807fb0c1f9f1480aabc91baa5adf35309451a
Ciastex <mrciastexx@gmail.com>
2015-02-02 11:59:58 +0100

    Added Message and UserAgent classes.

---
+++ ObcyProtoRev/Protocol/Client/UserAgent.cs
@@ -0,0 +1,19 @@
+﻿namespace ObcyProtoRev.Protocol.Client
+{
+    public class UserAgent
+    {
+        public string Name { get; private set; }
+        public string Version { get; private set; }
+
+        public UserAgent(string name, string version)
+        {
+            Name = name;
+            Version = version;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, version {1}");
+        }
+    }
+}
