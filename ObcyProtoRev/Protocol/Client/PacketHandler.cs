65194b52bdcd540e30630ef0b5930c7a02ade2bf
Ciastex <mrciastexx@gmail.com>
2015-02-03 17:32:13 +0100

    Written PacketHandler class logic.

--- ObcyProtoRev/Protocol/Client/PacketHandler.cs
+++ ObcyProtoRev/Protocol/Client/PacketHandler.cs
@@ -1,19 +1,72 @@
-﻿using ObcyProtoRev.Protocol.SockJs;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client
 {
-    class PacketHandler
+    static class WebsocketPacketHandler
     {
-        private Connection UnderlyingConnection { get; set; }
+        public delegate void ConnectionOpenEventHandler(string sockJsPacket);
+        public delegate void ConnectionCloseEventHandler(string sockJsPacket);
+        public delegate void SocketHeartbeatEventHandler(EventArgs e);
+        public delegate void SocketMessageEventHandler(List<Packet> packets);
 
-        public PacketHandler(Connection underlyingConnection)
+        public static event ConnectionOpenEventHandler ConnectionOpenPacketReceived;
+        public static event ConnectionCloseEventHandler ConnectionClosePacketReceived;
+        public static event SocketHeartbeatEventHandler SocketHeartbeatReceived;
+        public static event SocketMessageEventHandler SocketMessageReceived;
+
+        public static void HandlePacket(string sockJsPacket)
         {
-            UnderlyingConnection = underlyingConnection;
+            PacketType packetType = Decoder.DeterminePacketType(sockJsPacket);
+
+            switch (packetType)
+            {
+                case PacketType.ConnectionOpen:
+                    OnConnectionOpenPacketReceived(sockJsPacket);
+                    break;
+                case PacketType.ConnectionClose:
+                    OnConnectionClosePacketReceived(sockJsPacket);
+                    break;
+                case PacketType.SocketHeartbeat:
+                    OnSocketHeartbeatReceived();
+                    break;
+                case PacketType.SocketMessage:
+                    var packets = Decoder.DecodePackets(sockJsPacket);
+                    OnSocketMessageReceived(packets);
+                    break;
+                case PacketType.BinaryData:
+                    Debug.WriteLine("Binary data not supported.");
+                    break;
+                case PacketType.Invalid:
+                    Debug.WriteLine("Invalid packet received.");
+                    break;
+            }
         }
 
-        public void HandlePacket(Packet packet)
+        private static void OnConnectionOpenPacketReceived(string sockJsPacket)
         {
-            
+            var handler = ConnectionOpenPacketReceived;
+            if (handler != null) handler(sockJsPacket);
+        }
+
+        private static void OnConnectionClosePacketReceived(string sockJsPacket)
+        {
+            var handler = ConnectionClosePacketReceived;
+            if (handler != null) handler(sockJsPacket);
+        }
+
+        private static void OnSocketHeartbeatReceived()
+        {
+            var handler = SocketHeartbeatReceived;
+            if (handler != null) handler(EventArgs.Empty);
+        }
+
+        private static void OnSocketMessageReceived(List<Packet> packets)
+        {
+            var handler = SocketMessageReceived;
+            if (handler != null) handler(packets);
         }
     }
 }
