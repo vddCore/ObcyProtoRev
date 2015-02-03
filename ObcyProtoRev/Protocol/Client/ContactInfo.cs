34f25d0a2fbd127dda10efa6f4bd2a775c1c0226
Ciastex <mrciastexx@gmail.com>
2015-02-03 18:45:10 +0100

    Added ContactInfo class.

---
+++ ObcyProtoRev/Protocol/Client/ContactInfo.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace ObcyProtoRev.Protocol.Client
+{
+    public class ContactInfo
+    {
+        public int ClientID { get; private set; }
+        public string UID { get; private set; }
+        public JToken Preferences { get; private set; }
+        public bool FlaggedAsUnpleasant { get; private set; }
+
+        public ContactInfo(int clientId, string uid, JToken preferences, bool flaggedAsUnpleasant)
+        {
+            ClientID = clientId;
+            UID = uid;
+            Preferences = preferences;
+            FlaggedAsUnpleasant = flaggedAsUnpleasant;
+        }
+    }
+}
