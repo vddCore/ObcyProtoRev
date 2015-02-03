029cdb0b0c19e8627bedc31d19a3cd53b2e3cca4
Ciastex <mrciastexx@gmail.com>
2015-02-03 13:37:09 +0100

    Started extending Connection class to provide actual connection abilities.

--- ObcyProtoRev/Protocol/Connection.cs
+++ ObcyProtoRev/Protocol/Connection.cs
@@ -1,14 +1,39 @@
-﻿using WebSocketSharp;
+﻿using System;
+using ObcyProtoRev.Protocol.SockJs;
+using WebSocketSharp;
 
 namespace ObcyProtoRev.Protocol
 {
     public class Connection
     {
         private WebSocket WebSocket { get; set; }
+        private TargetWebsocketAddress WebsocketAddress { get; set; }
 
         public Connection()
         {
-            
+            RenewConnectionAddress();
+
+            WebSocket = new WebSocket(WebsocketAddress)
+            {
+                Origin = WebsocketAddress.Origin
+            };
+            WebSocket.OnOpen += WebSocket_OnOpen;
+            WebSocket.OnMessage += WebSocketOnOnMessage;
+        }
+
+        public void RenewConnectionAddress()
+        {
+            WebsocketAddress = new TargetWebsocketAddress();
+        }
+
+        private void WebSocket_OnOpen(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WebSocketOnOnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            throw new NotImplementedException();
         }
     }
 }
