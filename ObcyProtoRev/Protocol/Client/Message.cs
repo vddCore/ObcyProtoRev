5221cf30fc3182547f7df64c75c71380365cfefa
Ciastex <mrciastexx@gmail.com>
2015-02-03 18:05:24 +0100

    Made fields ClientID and PostID fields nullable.

--- ObcyProtoRev/Protocol/Client/Message.cs
+++ ObcyProtoRev/Protocol/Client/Message.cs
@@ -5,16 +5,16 @@
         public MessageType Type { get; private set; }
 
         public string Body { get; private set; }
-        public int ClientId { get; private set; }
-        public int PostId { get; private set; }
+        public int? ClientID { get; private set; }
+        public int? PostID { get; private set; }
 
         public Message(MessageType type, string body, int clientId, int postId)
         {
             Type = type;
 
             Body = body;
-            ClientId = clientId;
-            PostId = postId;
+            ClientID = clientId;
+            PostID = postId;
         }
     }
 }
