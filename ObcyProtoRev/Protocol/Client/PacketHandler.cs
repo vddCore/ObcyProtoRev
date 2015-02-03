ab2815bd636155f70c3534f4b599727ea9dfc806
Ciastex <mrciastexx@gmail.com>
2015-02-03 20:32:34 +0100

    Changed some event handler parameters.

--- ObcyProtoRev/Protocol/Client/PacketHandler.cs
+++ ObcyProtoRev/Protocol/Client/PacketHandler.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+
 using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client
 {
     static class WebsocketPacketHandler
     {
-        public delegate void ConnectionOpenEventHandler(string sockJsPacket);
-        public delegate void ConnectionCloseEventHandler(string sockJsPacket);
-        public delegate void SocketHeartbeatEventHandler(EventArgs e);
+        public delegate void ConnectionOpenEventHandler(EventArgs e);
+        public delegate void ConnectionCloseEventHandler(EventArgs e);
+        public delegate void SocketHeartbeatEventHandler(DateTime heartbeatTime);
         public delegate void SocketMessageEventHandler(List<Packet> packets);
 
         public static event ConnectionOpenEventHandler ConnectionOpenPacketReceived;
@@ -24,13 +25,13 @@ namespace ObcyProtoRev.Protocol.Client
             switch (packetType)
             {
                 case PacketType.ConnectionOpen:
-                    OnConnectionOpenPacketReceived(sockJsPacket);
+                    OnConnectionOpenPacketReceived();
                     break;
                 case PacketType.ConnectionClose:
-                    OnConnectionClosePacketReceived(sockJsPacket);
+                    OnConnectionClosePacketReceived();
                     break;
                 case PacketType.SocketHeartbeat:
-                    OnSocketHeartbeatReceived();
+                    OnSocketHeartbeatReceived(DateTime.Now);
                     break;
                 case PacketType.SocketMessage:
                     var packets = Decoder.DecodePackets(sockJsPacket);
@@ -45,22 +46,22 @@ namespace ObcyProtoRev.Protocol.Client
             }
         }
 
-        private static void OnConnectionOpenPacketReceived(string sockJsPacket)
+        private static void OnConnectionOpenPacketReceived()
         {
             var handler = ConnectionOpenPacketReceived;
-            if (handler != null) handler(sockJsPacket);
+            if (handler != null) handler(EventArgs.Empty);
         }
 
-        private static void OnConnectionClosePacketReceived(string sockJsPacket)
+        private static void OnConnectionClosePacketReceived()
         {
             var handler = ConnectionClosePacketReceived;
-            if (handler != null) handler(sockJsPacket);
+            if (handler != null) handler(EventArgs.Empty);
         }
 
-        private static void OnSocketHeartbeatReceived()
+        private static void OnSocketHeartbeatReceived(DateTime heartbeatTime)
         {
             var handler = SocketHeartbeatReceived;
-            if (handler != null) handler(EventArgs.Empty);
+            if (handler != null) handler(heartbeatTime);
         }
 
         private static void OnSocketMessageReceived(List<Packet> packets)
