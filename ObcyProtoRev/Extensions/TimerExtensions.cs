0d7bf89d0b068acf2010052ae9768c15e2afabad
Tomasz Cichoń <mrciastexx@gmail.com>
2015-02-19 13:00:08 +0100

    Added TimerExtensions extension class.

---
+++ ObcyProtoRev/Extensions/TimerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Timers;
+
+namespace ObcyProtoRev.Extensions
+{
+    public static class TimerExtensions
+    {
+        public static void Reset(this Timer timer)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+    }
+}
