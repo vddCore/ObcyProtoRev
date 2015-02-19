a4088cdf5edb750df92dfec57458f00d25346eb6
Tomasz Cichoń <mrciastexx@gmail.com>
2015-02-19 12:59:23 +0100

    Developed reconnect request packet and reconnect info classes.

--- ObcyProtoRev/Protocol/Client/Packets/ReconnectRequestPacket.cs
+++ ObcyProtoRev/Protocol/Client/Packets/ReconnectRequestPacket.cs
@@ -1,14 +1,31 @@
-﻿using ObcyProtoRev.Protocol.SockJs;
+﻿using System;
+using Newtonsoft.Json.Linq;
+using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client.Packets
 {
     class ReconnectRequestPacket : Packet
     {
+        private const string UaStringFormat = "{0}\"{1}\":true,\"version\":\"{2}\"{3}";
+
         public ReconnectRequestPacket(ReconnectInfo reconnectInfo)
         {
             Header = "_reconn_me";
 
-            //TODO: Stub
+            var connectionStateString = reconnectInfo.ClientState == ClientState.Chatting ? 
+                                                                             "while conv" : 
+                                                                             "out of conv";
+
+            var time = (DateTime.Now - reconnectInfo.ConnectionTime).TotalMilliseconds;
+            var uaString = string.Format(UaStringFormat, "{", reconnectInfo.UserAgent.Name, reconnectInfo.UserAgent.Version, "}");
+
+            Data = new JObject();
+            Data["log_msg"] = string.Format("{0}, time:{1} transports: websocket > websocket, browser: {2}", connectionStateString, time, uaString);
+            Data["ckey"] = reconnectInfo.CurrentContactUID;
+            Data["last_conn_id"] = reconnectInfo.LastConnectionUID;
+            Data["last_post_id"] = reconnectInfo.LastPostID;
+            Data["prev_ckey"] = reconnectInfo.PreviousContactUID;
+            Data["mobile"] = reconnectInfo.IsMobile;
         }
     }
 }
--- ObcyProtoRev/Protocol/Client/ReconnectInfo.cs
+++ ObcyProtoRev/Protocol/Client/ReconnectInfo.cs
@@ -1,7 +1,59 @@
-﻿namespace ObcyProtoRev.Protocol.Client
+﻿using System;
+
+namespace ObcyProtoRev.Protocol.Client
 {
     public class ReconnectInfo
     {
-        //TODO: Stub
+        /// <summary>
+        /// Gets a value indicating whether request is being sent from a mobile device.
+        /// </summary>
+        public bool IsMobile { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing client state at the time of connection loss.
+        /// </summary>
+        public ClientState ClientState { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing session creation time.
+        /// </summary>
+        public DateTime ConnectionTime { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing last message ID.
+        /// </summary>
+        public int LastPostID { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing previous stranger UID.
+        /// </summary>
+        public string PreviousContactUID { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing current stranger UID.
+        /// </summary>
+        public string CurrentContactUID { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing last assigned UID.
+        /// </summary>
+        public string LastConnectionUID { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing UserAgent used at the time of connection loss.
+        /// </summary>
+        public UserAgent UserAgent { get; private set; }
+
+        public ReconnectInfo(bool isMobile, ClientState clientState, DateTime connectionTime, int lastPostID, string prevContactUID, string currentContactUID, string lastConnectionUID, UserAgent userAgent)
+        {
+            IsMobile = isMobile;
+            ClientState = clientState;
+            ConnectionTime = connectionTime;
+            LastPostID = lastPostID;
+            PreviousContactUID = prevContactUID;
+            CurrentContactUID = currentContactUID;
+            LastConnectionUID = lastConnectionUID;
+            UserAgent = userAgent;
+        }
     }
 }
