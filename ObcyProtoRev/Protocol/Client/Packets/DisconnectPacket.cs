4ebec425e400d2df39399786889a7cfab3d2cb64
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Documented DisconnectPacket class. Is now sealed.

--- ObcyProtoRev/Protocol/Client/Packets/DisconnectPacket.cs
+++ ObcyProtoRev/Protocol/Client/Packets/DisconnectPacket.cs
@@ -3,14 +3,21 @@ using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client.Packets
 {
-    class DisconnectPacket : Packet
+    /// <summary>
+    /// Represents a disconnect request packet which is ready to be sent as-is. This class cannot be inherited.
+    /// </summary>
+    public sealed class DisconnectPacket : Packet
     {
-        public DisconnectPacket(string contactGuid)
+        /// <summary>
+        /// Creates a new instance of DisconnectPacket class.
+        /// </summary>
+        /// <param name="strangerUID">UID of a currently connected stranger.</param>
+        public DisconnectPacket(string strangerUID)
         {
             Header = "_distalk";
 
             Data = new JObject();
-            Data["ckey"] = contactGuid;
+            Data["ckey"] = strangerUID;
         }
     }
 }
