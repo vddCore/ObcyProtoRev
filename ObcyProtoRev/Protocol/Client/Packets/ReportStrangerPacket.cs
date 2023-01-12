213cc7e247e493c6f9616fb5dd220a12e6d077ea
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Documented ReportStrangerPacket class. Is now sealed.

--- ObcyProtoRev/Protocol/Client/Packets/ReportStrangerPacket.cs
+++ ObcyProtoRev/Protocol/Client/Packets/ReportStrangerPacket.cs
@@ -3,14 +3,21 @@ using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client.Packets
 {
-    class ReportStrangerPacket : Packet
+    /// <summary>
+    /// Represents a report-as-unpleasant packet which is ready to be sent as-is. This class cannot be inherited.
+    /// </summary>
+    public sealed class ReportStrangerPacket : Packet
     {
-        public ReportStrangerPacket(string clientGuid)
+        /// <summary>
+        /// Creates a new instance of ReportStrangerPacket class.
+        /// </summary>
+        /// <param name="strangerUID">UID of a stranger which we want to report.</param>
+        public ReportStrangerPacket(string strangerUID)
         {
             Header = "_reptalk";
 
             Data = new JObject();
-            Data["ckey"] = clientGuid;
+            Data["ckey"] = strangerUID;
         }
     }
 }
