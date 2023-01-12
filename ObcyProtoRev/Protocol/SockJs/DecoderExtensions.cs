4e8f914e4c8acff3ab41d925bd6630d72577ed24
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Added SockJS packet decoder class.

---
+++ ObcyProtoRev/Protocol/SockJs/DecoderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ObcyProtoRev.Protocol.SockJs
+{
+    static class DecoderExtensions
+    {
+        public static Packet GetFirstPacket(this List<Packet> packets)
+        {
+            return packets.FirstOrDefault();
+        }
+    }
+}
