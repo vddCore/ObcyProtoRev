99067b5a741c5bed4c4bb6fca39630b63426f835
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Added Pong and ClientInfo packet classes.

---
+++ ObcyProtoRev/Protocol/Client/Packets/ClientInfoPacket.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using ObcyProtoRev.Protocol.SockJs;
+
+namespace ObcyProtoRev.Protocol.Client.Packets
+{
+    class ClientInfoPacket : Packet
+    {
+        public ClientInfoPacket(bool isMobile, UserAgent userAgent)
+        {
+            Header = "_cinfo";
+
+            var jObject = new JObject();
+            jObject["mobile"] = isMobile;
+            jObject["cver"] = userAgent.ToString();
+
+            Data = jObject;
+        }
+    }
+}
