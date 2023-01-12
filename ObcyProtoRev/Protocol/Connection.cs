ee58f53df3c6b5139bb878d492ccb19029309d28
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Updated project file, added Connection class.

---
+++ ObcyProtoRev/Protocol/Connection.cs
@@ -0,0 +1,15 @@
+﻿using WebSocketSharp;
+using ObcyProtoRev.Protocol.SockJs;
+
+namespace ObcyProtoRev.Protocol
+{
+    public class Connection
+    {
+        private WebSocket WebSocket { get; set; }
+
+        public Connection()
+        {
+            
+        }
+    }
+}
