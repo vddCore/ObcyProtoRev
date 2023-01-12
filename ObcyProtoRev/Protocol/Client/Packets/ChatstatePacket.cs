7f883cbdb1e717fa05597589b3b0d202c97833bd
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Documented ChatstatePacket class, renamed constructor parameter.

--- ObcyProtoRev/Protocol/Client/Packets/ChatstatePacket.cs
+++ ObcyProtoRev/Protocol/Client/Packets/ChatstatePacket.cs
@@ -3,14 +3,22 @@ using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client.Packets
 {
-    class ChatstatePacket : Packet
+    /// <summary>
+    /// Represents a chatstate packet which is ready to be sent as-is.
+    /// </summary>
+    public sealed class ChatstatePacket : Packet
     {
-        public ChatstatePacket(bool typing, string contactGuid)
+        /// <summary>
+        /// Creates a new instance of ChatstatePacket class.
+        /// </summary>
+        /// <param name="typing">A value indicating whether or not this packet means that client is typing.</param>
+        /// <param name="strangerUID">A value describing target stranger UID.</param>
+        public ChatstatePacket(bool typing, string strangerUID)
         {
             Header = "_mtyp";
 
             Data = new JObject();
-            Data["ckey"] = contactGuid;
+            Data["ckey"] = strangerUID;
             Data["val"] = typing;
         }
     }
