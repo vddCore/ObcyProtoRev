9db5c5ac525cb0e34348968acc563905b71978c2
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Documented PongPacket class. Is now sealed.

--- ObcyProtoRev/Protocol/Client/Packets/PongPacket.cs
+++ ObcyProtoRev/Protocol/Client/Packets/PongPacket.cs
@@ -2,8 +2,14 @@
 
 namespace ObcyProtoRev.Protocol.Client.Packets
 {
-    class PongPacket : Packet
+    /// <summary>
+    /// Represents a pong packet which is ready to be sent as-is. This class cannot be inherited.
+    /// </summary>
+    public sealed class PongPacket : Packet
     {
+        /// <summary>
+        /// Creates a new instance of PongPacket class.
+        /// </summary>
         public PongPacket()
         {
             Header = "_gdzie";
