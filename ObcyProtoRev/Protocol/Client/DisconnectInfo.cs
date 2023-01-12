c3ae19a290f2ad1f8ddddb159e705758b30ef289
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Documented DisconnectInfo class. Is now sealed.

--- ObcyProtoRev/Protocol/Client/DisconnectInfo.cs
+++ ObcyProtoRev/Protocol/Client/DisconnectInfo.cs
@@ -1,10 +1,25 @@
 ﻿namespace ObcyProtoRev.Protocol.Client
 {
-    public class DisconnectInfo
+    /// <summary>
+    /// Represents information received at the moment of disconnect event. This class cannot be inherited.
+    /// </summary>
+    public sealed class DisconnectInfo
     {
+        /// <summary>
+        /// Gets a value indicating that disconnect event was fired outside a conversation.
+        /// </summary>
         public bool IsReminder { get; private set; }
+
+        /// <summary>
+        /// Gets a value describing ClientID of stranger who caused the disconnect event to occur.
+        /// </summary>
         public int ClientID { get; private set; }
 
+        /// <summary>
+        /// Creates a new instance of DisconnectInfo class.
+        /// </summary>
+        /// <param name="isReminder">Whether or not an event using this information is fired outside a conversation.</param>
+        /// <param name="clientId">ClientID of a stranger who is causing the event to occur.</param>
         public DisconnectInfo(bool isReminder, int clientId)
         {
             IsReminder = isReminder;
