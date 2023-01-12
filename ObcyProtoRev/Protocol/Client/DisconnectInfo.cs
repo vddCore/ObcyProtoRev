152cff88b31e52406ee4071a52c0f91d52f0cc09
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Added DisconnectInfo class.

---
+++ ObcyProtoRev/Protocol/Client/DisconnectInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ObcyProtoRev.Protocol.Client
+{
+    public class DisconnectInfo
+    {
+        public bool IsReminder { get; private set; }
+        public int ClientID { get; private set; }
+
+        public DisconnectInfo(bool isReminder, int clientId)
+        {
+            IsReminder = isReminder;
+            ClientID = clientId;
+        }
+    }
+}
