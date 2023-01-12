92554e54424bf481fe9a64e52c733e8b26c47012
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Added identity classes as well as stranger search initiation packet.

---
+++ ObcyProtoRev/Protocol/Client/Identity/PersonInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ObcyProtoRev.Protocol.Client.Identity
+{
+    class PersonInfo
+    {
+        public int Sex { get; private set; }
+        public Location Location { get; private set; }
+
+        public PersonInfo(int sex, Location location)
+        {
+            Sex = sex;
+            Location = location;
+        }
+    }
+}
