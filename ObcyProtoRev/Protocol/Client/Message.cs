8d945dbc3cae82988e7ff0a70c4baefdfc769e5d
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Added Message and UserAgent classes.

---
+++ ObcyProtoRev/Protocol/Client/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace ObcyProtoRev.Protocol.Client
+{
+    public class Message
+    {
+        public string Body { get; private set; }
+        public int ClientId { get; private set; }
+        public int PostId { get; private set; }
+
+        public Message(string body, int clientId, int postId)
+        {
+            Body = body;
+            ClientId = clientId;
+            PostId = postId;
+        }
+    }
+}
