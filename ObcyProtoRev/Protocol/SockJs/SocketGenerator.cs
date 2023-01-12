d10bc6c60e49a324cd13f23c6f1423baa9955313
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Added SockJS compatible socket URL generation class.

---
+++ ObcyProtoRev/Protocol/SockJs/SocketGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ObcyProtoRev.Protocol.SockJs
+{
+    static class SocketGenerator
+    {
+        public static string GenerateRandomSocketNumber()
+        {
+            var sUid = new Random().Next(0, 999);
+
+            switch (sUid.ToString().Length)
+            {
+                case 1:
+                    return sUid.ToString("00");
+                case 2:
+                    return sUid.ToString("0");
+                default:
+                    return sUid.ToString();
+            }
+        }
+
+        public static string GenerateRandomSocketSeed(int length)
+        {
+            var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_".ToCharArray();
+            var data = new byte[1];
+            var crypto = new RNGCryptoServiceProvider();
+
+            crypto.GetNonZeroBytes(data);
+            data = new byte[length];
+
+            crypto.GetNonZeroBytes(data);
+            var result = new StringBuilder(length);
+
+            foreach (var b in data)
+            {
+                result.Append(characters[b % (characters.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
