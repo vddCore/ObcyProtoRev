737ba8ce8dac27e003687a47d5270028fc37208a
vddCore <vddcore@outlook.com>
2023-01-12 10:07:39 +0100

    Documented RandomTopicPacket class. Is now sealed.

--- ObcyProtoRev/Protocol/Client/Packets/RandomTopicPacket.cs
+++ ObcyProtoRev/Protocol/Client/Packets/RandomTopicPacket.cs
@@ -3,14 +3,21 @@ using ObcyProtoRev.Protocol.SockJs;
 
 namespace ObcyProtoRev.Protocol.Client.Packets
 {
-    class RandomTopicPacket : Packet
+    /// <summary>
+    /// Represents a random topic request packet which is ready to be sent as-is. This class cannot be inherited.
+    /// </summary>
+    public sealed class RandomTopicPacket : Packet
     {
-        public RandomTopicPacket(string contactGuid)
+        /// <summary>
+        /// Creates a new instance of RandomTopicPacket class.
+        /// </summary>
+        /// <param name="strangerUID">UID of a currently connected stranger.</param>
+        public RandomTopicPacket(string strangerUID)
         {
             Header = "_randtopic";
 
             Data = new JObject();
-            Data["ckey"] = contactGuid;
+            Data["ckey"] = strangerUID;
         }
     }
 }
